3fdc0d285bc169d917f676ca8d80991d541f39db
Iain04 <82942451+Iain04@users.noreply.github.com>
2022-08-16 16:09:41 +0800

    Update signin.js

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,6 +1,6 @@
 function signinUser() {
     var request =  new XMLHttpRequest();
-    request.open("POST", "http://127.0.0.1:8080/login", true);
+    request.open("POST", "http://ec2-54-163-18-85.compute-1.amazonaws.com:8080/login", true);
     request.setRequestHeader("Content-Type","application/json");
     request.onload = function() {
         $('#signup-modal').modal('hide');
