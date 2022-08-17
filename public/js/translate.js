a37875a7f3c551df754279096e2e1a1c057dc21d
Iain04 <82942451+Iain04@users.noreply.github.com>
2022-08-18 03:40:46 +0800

    Update translate.js

--- public/js/translate.js
+++ public/js/translate.js
@@ -1,18 +1,14 @@
 function translateAbout() {
-    if (sessionStorage.getItem("language") != null) {
-        var language = sessionStorage.getItem('language');
+    var langauage = document.getElementById("languages").value;
 
-        var request = new XMLHttpRequest();
-        request.open("POST", "https://kuobo8gmpf.execute-api.us-east-1.amazonaws.com/default/translate", true); 
-        request.setRequestHeader("Content-Type", "application/json");
-        request.onload = function () {
-            console.log("translated");
-            translatedText = JSON.parse(request.responseText);
-            console.log(translatedText);
-        };
-        request.send(JSON.stringify(language));
-    }
-    else {
-        $('#fail-modal').modal('show');
-    }
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://kuobo8gmpf.execute-api.us-east-1.amazonaws.com/default/translate", true); 
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function () {
+        var payload = {langauge:langauges};
+        console.log("translated");
+        translatedText = JSON.parse(request.responseText);
+        console.log(translatedText);
+    };
+    request.send(JSON.stringify(payload));
 }
