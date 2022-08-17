53086f5b86416dd0b6e25f1d5293bed658707e38
Iain04 <82942451+Iain04@users.noreply.github.com>
2022-08-18 01:00:53 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
 
-    host:'restaurant-db.cojyialevtxf.us-east-1.rds.amazonaws.com',
+    host:'restaurant-db.c0c8awp9pzix.us-east-1.rds.amazonaws.com',
 
     port: '3306',
 
