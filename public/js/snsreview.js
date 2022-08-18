f26c1fd46f3c5e252a5a928dda6c7037eba22005
Iain04 <82942451+Iain04@users.noreply.github.com>
2022-08-18 21:42:44 +0800

    Create snsreview.js

---
+++ public/js/snsreview.js
@@ -0,0 +1,17 @@
+function notification(){
+
+    var notification = new XMLHttpRequest();
+    console.log("notified")
+    notification.open("POST", "https://qho62h7g7k.execute-api.us-east-1.amazonaws.com/default/snsreview,true);
+    notification.setRequestHeader("Content-Type", "application/json");
+
+    restaurantname = localStorage.getItem("restaurant-name");
+    userid = sessionStorage.getItem("idUser");
+    username = sessionStorage.getItem("userName");
+    reviewmessage = document.getElementById("userComments").value;
+
+    var payload = {restaurantname:restaurantname,userid:userid,username:username,reviewmessage:reviewmessage};
+    console.log(payload)
+    notification.send(JSON.stringify(payload));
+
+}
