132ffe983403a59ae2031b19e6d9eb6946b38912
Iain04 <82942451+Iain04@users.noreply.github.com>
2022-08-18 17:45:24 +0800

    Update txtspeech.js

--- public/js/txtspeech.js
+++ public/js/txtspeech.js
@@ -1,10 +1,19 @@
 (function() {
   const elemPlayButton = document.querySelector("#play-button")
+  const elemVoice = document.querySelector("#selectLan");
   const elemText = document.querySelector("#aboutText")
   
   elemPlayButton.addEventListener("click", function() {
+    if (elemVoice.value == "english") {
+      var voice = "Joanna"
+    }
+    
+    if (elemVoice.value == "italian") {
+      var voice = "Bianca"
+    }
+    
     let url = "https://90sq38y7g7.execute-api.us-east-1.amazonaws.com/default/polly"
-    url += "?voice=Joanna";
+    url += "?voice=" + encodeURIComponent(voice);
     url += "&text=" + encodeURIComponent(elemText.textContent);
     
     const elemAudio = document.createElement("AUDIO");
